75f69aa19dcb51742deaf367f905bdf128817b53
Curtis-Lane <129118489+Curtis-Lane@users.noreply.github.com>
2023-10-03 13:25:16 -0600

    Change rendering to use OpenGL and set up a basic scene using it.

--- Source/Application/World01.cpp
+++ Source/Application/World01.cpp
@@ -13,16 +13,46 @@ namespace nc {
 	}
 
 	void World01::Update(float deltaTime) {
+		this->position.x += ENGINE.GetSystem<InputSystem>()->GetKeyDown(SDL_SCANCODE_D) ? deltaTime : 0;
+		this->position.x += ENGINE.GetSystem<InputSystem>()->GetKeyDown(SDL_SCANCODE_A) ? -deltaTime : 0;
+
+		this->position.y += ENGINE.GetSystem<InputSystem>()->GetKeyDown(SDL_SCANCODE_W) ? deltaTime : 0;
+		this->position.y += ENGINE.GetSystem<InputSystem>()->GetKeyDown(SDL_SCANCODE_S) ? -deltaTime : 0;
+
+		this->angle += ENGINE.GetSystem<InputSystem>()->GetKeyDown(SDL_SCANCODE_LEFT) ? deltaTime * 90 : 0;
+		this->angle += ENGINE.GetSystem<InputSystem>()->GetKeyDown(SDL_SCANCODE_RIGHT) ? -deltaTime * 90 : 0;
+
+		this->time += deltaTime;
 	}
 
 	void World01::Draw(Renderer& renderer) {
 		// pre-render
-		renderer.SetColor(0, 0, 0, 0);
 		renderer.BeginFrame();
 
 		// render
-		renderer.SetColor(255, 255, 255, 255);
-		renderer.DrawPoint(random(renderer.GetWidth()), random(renderer.GetHeight()));
+		glPushMatrix();
+
+		glTranslatef(this->position.x, this->position.y, 0.0f);
+		glRotatef(this->angle, 0.0f, 0.0f, 1.0f);
+		glScalef(Clamp(((std::sinf(this->time * 5) + 1) * 0.5f), 0.01f, 1.0f), 1.0f, 1.0f);
+
+		glBegin(GL_QUADS);
+
+		glColor3f(1.0f, 0.0f, 0.0f);
+		glVertex2f(-0.5f, -0.5f);
+
+		glColor3f(0.0f, 1.0f, 0.0f);
+		glVertex2f(-0.5f, 0.5f);
+
+		glColor3f(0.0f, 0.0f, 1.0f);
+		glVertex2f(0.5f, 0.5f);
+
+		glColor3f(1.0f, 1.0f, 0.0f);
+		glVertex2f(0.5f, -0.5f);
+
+		glEnd();
+
+		glPopMatrix();
 
 		// post-render
 		renderer.EndFrame();
