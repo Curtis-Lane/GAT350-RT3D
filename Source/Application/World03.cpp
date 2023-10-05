a83b61537dd5896222d6f71c097c64853959f5c9
Curtis-Lane <129118489+Curtis-Lane@users.noreply.github.com>
2023-10-05 11:57:13 -0600

    Add a new scene demonstrating the use of shader files

--- Source/Application/World03.cpp
+++ Source/Application/World03.cpp
@@ -1,4 +1,4 @@
-#include "World02.h"
+#include "World03.h"
 
 #include "Framework/Framework.h"
 #include "Input/InputSystem.h"
@@ -6,39 +6,10 @@
 #define INTERLEAVE
 
 namespace nc {
-	bool World02::Initialize() {
+	bool World03::Initialize() {
 		// Shaders
-		const char* vertexShader =
-			"#version 430\n"
-			"layout (location=0) in vec3 position;"
-			"layout (location=1) in vec3 color;"
-			"layout (location=0) out vec3 ocolor;"
-			"void main() {"
-			"  ocolor = color;"
-			"  gl_Position = vec4(position, 1.0);"
-			"}";
-
-		const char* fragmentShader =
-			"#version 430\n"
-			"layout (location=0) in vec3 color;"
-			"out vec4 ocolor;"
-			"void main() {"
-			"  ocolor = vec4(color, 1);"
-			"}";
-
-		GLuint vs = glCreateShader(GL_VERTEX_SHADER);
-		glShaderSource(vs, 1, &vertexShader, NULL);
-		glCompileShader(vs);
-
-		GLuint fs = glCreateShader(GL_FRAGMENT_SHADER);
-		glShaderSource(fs, 1, &fragmentShader, NULL);
-		glCompileShader(fs);
-
-		GLuint program = glCreateProgram();
-		glAttachShader(program, vs);
-		glAttachShader(program, fs);
-		glLinkProgram(program);
-		glUseProgram(program);
+		this->program = GET_RESOURCE(Program, "Shaders/unlit_color.prog");
+		this->program->Use();
 
 		#ifdef INTERLEAVE
 		
@@ -122,25 +93,17 @@ namespace nc {
 		return true;
 	}
 
-	void World02::Shutdown() {
+	void World03::Shutdown() {
 	}
 
-	void World02::Update(float deltaTime) {
-		/*
-		this->position.x += ENGINE.GetSystem<InputSystem>()->GetKeyDown(SDL_SCANCODE_D) ? deltaTime : 0;
-		this->position.x += ENGINE.GetSystem<InputSystem>()->GetKeyDown(SDL_SCANCODE_A) ? -deltaTime : 0;
-
-		this->position.y += ENGINE.GetSystem<InputSystem>()->GetKeyDown(SDL_SCANCODE_W) ? deltaTime : 0;
-		this->position.y += ENGINE.GetSystem<InputSystem>()->GetKeyDown(SDL_SCANCODE_S) ? -deltaTime : 0;
-
-		this->angle += ENGINE.GetSystem<InputSystem>()->GetKeyDown(SDL_SCANCODE_LEFT) ? deltaTime * 90 : 0;
-		this->angle += ENGINE.GetSystem<InputSystem>()->GetKeyDown(SDL_SCANCODE_RIGHT) ? -deltaTime * 90 : 0;
-
+	void World03::Update(float deltaTime) {
 		this->time += deltaTime;
-		*/
+
+		GLint uniform = glGetUniformLocation(this->program->m_program, "time");
+		glUniform1f(uniform, this->time);
 	}
 
-	void World02::Draw(Renderer& renderer) {
+	void World03::Draw(Renderer& renderer) {
 		// pre-render
 		renderer.BeginFrame();
 
---
+++ Source/Application/World03.h
@@ -0,0 +1,23 @@
+#pragma once
+
+#include "Framework/World.h"
+#include "Core/Math/Vector2.h"
+#include "Renderer/Renderer.h"
+
+namespace nc {
+	class World03 : public World {
+		public:
+			bool Initialize() override;
+			void Shutdown() override;
+			void Update(float deltaTime) override;
+			void Draw(Renderer& renderer) override;
+
+		private:
+			//float angle = 0.0f;
+			float time;
+			//Vector2 position;
+
+			GLuint vao;
+			res_t<Program> program;
+	};
+}
